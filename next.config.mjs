4287ce2deedfabd92d6fd82a11fea10d1516859d
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-09-30 22:09:37 +0900

    first commit

---
+++ next.config.mjs
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: '/member_personal_webpages',
+  assetPrefix: '/member_personal_webpages/',
+  reactStrictMode: true,
+  swcMinify: true,
+};
+
+export default nextConfig;
