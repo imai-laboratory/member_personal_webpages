8b5d0e93e53e7dc92311def41c92c8e7d6fb9f59
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-10-02 00:11:41 +0900

    import訂正

--- components/index.js
+++ components/index.js
@@ -1,8 +1,22 @@
-export { default as Header } from './layout/Header';
-export { default as Profile } from './sections/Profile';
-export { default as Research } from './sections/Research';
-export { default as Card } from './ui/BaseCard';
-export { default as ResearchDemoCards } from './sections/ResearchDemoCards';
-export { default as Publications } from './sections/Publications';
+// Path: components/DesignSamples
+export { default as DesignA } from './DesignSamples/DesignA';
+
+// Path: components/layout/
+export { default as BaseLayout } from './layout/BaseLayout';
 export { default as Footer } from './layout/Footer';
+export { default as Header } from './layout/Header';
+
+// Path: components/sections/
+export { default as Profile } from './sections/Profile';
+export { default as Publications } from './sections/Publications';
+export { default as Research } from './sections/Research';
+export { default as ResearchDemoCards } from './sections/ResearchDemoCards';
+
+
+// Path: components/ui/
+export { default as BaseCard } from './ui/BaseCard';
+export { default as Publication } from './ui/Publication';
+export { default as SocialIcons } from './ui/SocialIcons';
 export { default as VideoPlayer } from './ui/VideoPlayer';
+
+
