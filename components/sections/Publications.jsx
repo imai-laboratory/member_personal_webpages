c403c87c2d8a62cab91162fa8e152f1dce688194
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-10-01 23:45:01 +0900

    ui修正

--- components/sections/Publications.jsx
+++ components/sections/Publications.jsx
@@ -3,8 +3,8 @@ import Publication from "@/components/ui/Publication";
 export default function Publications({ publications }) {
   return (
     <section className="flex flex-col gap-10 p-10 justify-center bg-white">
-      <h2 className="flex items-center text-3xl px-14 font-semibold">研究業績</h2>
-      <div className="px-20">
+      <h2 className="flex items-center text-3xl md:px-14 font-semibold">研究業績</h2>
+      <div className="px-10 md:px-20">
         {publications?.map((publication, index) => (
           <Publication key={index} publication={publication} />
         ))}
