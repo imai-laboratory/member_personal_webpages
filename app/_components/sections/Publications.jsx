564281a53d158ff72c3b50b8271fb6898c2e7a00
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-10-03 23:48:10 +0900

    [add] coming soon

--- app/_components/sections/Publications.jsx
+++ app/_components/sections/Publications.jsx
@@ -5,10 +5,15 @@ export default function Publications({ publications }) {
     <section id="publications" className="flex flex-col gap-10 p-10 justify-center bg-white">
       <h2 className="flex items-center text-3xl md:px-14 font-semibold">研究業績</h2>
       <div className="px-2 md:px-20">
-        {publications?.map((publication, index) => (
-          <Publication key={index} publication={publication} />
-        ))}
+        {publications && publications.length > 0 ? (
+          publications.map((publication, index) => (
+            <Publication key={index} publication={publication}/>
+          ))
+        ) : (
+          <p className="text-gray-950 font-bold text-center text-2xl my-6">Coming Soon...</p>
+        )}
       </div>
+
     </section>
   );
 }
--- app/_components/sections/ResearchDemoCards.jsx
+++ app/_components/sections/ResearchDemoCards.jsx
@@ -5,15 +5,19 @@ export default function ResearchDemoCards({ researchDemos }) {
     <section id="demo" className="flex flex-col gap-10 p-10 justify-center bg-customBlue">
       <h2 className="flex items-center text-3xl md:px-14 text-blue-500 font-semibold">研究紹介</h2>
       <div className="flex flex-col md:flex-row gap-10 justify-center items-center md:items-stretch">
-        {Object.entries(researchDemos)?.map(([key, researchDemo], index) => (
-          <BaseCard
-            key={index}
-            title={researchDemo.title}
-            description={researchDemo.description}
-            videoUrl={researchDemo.video}
-            thumbnailUrl={researchDemo.thumbnail}
-          />
-        ))}
+        {researchDemos && Object.keys(researchDemos).length > 0 ? (
+          Object.entries(researchDemos).map(([key, researchDemo], index) => (
+            <BaseCard
+              key={index}
+              title={researchDemo.title}
+              description={researchDemo.description}
+              videoUrl={researchDemo.video}
+              thumbnailUrl={researchDemo.thumbnail}
+            />
+          ))
+        ) : (
+          <p className="text-gray-950 font-bold text-2xl my-6">Coming Soon...</p>
+        )}
       </div>
     </section>
   );
