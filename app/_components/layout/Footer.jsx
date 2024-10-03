1ec29bb3b5cc1d3074b572ff8bc8df05554d4dc2
Kenshin0011 <nakanishi@ailab.ics.keio.ac.jp>
2024-10-03 21:59:11 +0900

    [fix] テキストサイズ

--- app/_components/layout/Footer.jsx
+++ app/_components/layout/Footer.jsx
@@ -1,8 +1,10 @@
 export default function Footer({ email }) {
   return (
-    <footer className="bg-gray-800 p-6 text-white text-center">
-      <p><strong>Contact:</strong> {email}</p>
-      <p>&copy; 2024 Imai Lab</p>
+    <footer className="flex flex-col gap-4 bg-gray-800 p-6 text-white text-center">
+      <p className="text-lg">
+        <strong>Contact:</strong> {email}
+      </p>
+      <p className="text-sm">&copy; 2024 Imai Lab</p>
     </footer>
   );
 }
